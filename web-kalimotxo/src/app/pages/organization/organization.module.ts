a7253196ba663d65b4e2ab41544bdd09875bf939
Bogdan Lupu <lupu60@gmail.com>
2018-11-03 00:30:44 +0200

    feat: add org dashboard component

--- web-kalimotxo/src/app/pages/organization/organization.module.ts
+++ web-kalimotxo/src/app/pages/organization/organization.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { OrganizationRoutingModule } from './organization-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
     OrganizationRoutingModule
   ],
-  declarations: []
+  declarations: [DashboardComponent]
 })
 export class OrganizationModule { }
