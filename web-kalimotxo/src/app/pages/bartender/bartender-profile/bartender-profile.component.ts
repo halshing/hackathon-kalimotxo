3e6637136efd4a5896490d533e481db372d9e84a
maudel <maudelaquintana@gmail.com>
2018-11-03 16:44:42 -0400

    bartender profile changes

--- web-kalimotxo/src/app/pages/bartender/bartender-profile/bartender-profile.component.ts
+++ web-kalimotxo/src/app/pages/bartender/bartender-profile/bartender-profile.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BartenderService } from '@app/services/bartender.service';
+import { LocalStorageService } from '@app/services/localStorage.service';
+import { NotificationService } from '@app/services/notification.service';
+import { first } from 'rxjs/operators';
+import { Bartender } from '../../../models/bartender';
+import { Rating } from '../../../models/rating';
+import { Router } from '@angular/router';
+
+import { RestUserService } from '@app/api/rest-user.service';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bartender-profile',
@@ -9,9 +19,68 @@ import { BartenderService } from '@app/services/bartender.service';
 })
 export class BartenderProfileComponent implements OnInit {
   bartenderId;
-  constructor(private route: ActivatedRoute, private bartenderService: BartenderService) {
+  bartender: Bartender;
+  ratings: Rating[];
+  bartenderProfileForm: FormGroup;
+  constructor(private route: ActivatedRoute,
+              private bartenderService: BartenderService,
+              private notificationService: NotificationService,
+              private localStorageService: LocalStorageService,
+              private restUserService: RestUserService,
+              private router: Router) {
     this.route.params.subscribe((params) => (this.bartenderId = params.bartenderId));
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.bartenderProfileForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', Validators.required),
+      userType: new FormControl('', Validators.required),
+      userId: new FormControl('', Validators.required)
+    });
+    this.bartenderService
+    .getBartenderById(this.bartenderId)
+    .pipe(first())
+    .subscribe(
+      (data: any) => {
+        if (data.result.error) {
+          this.notificationService.displayError({
+            name: data.result.error.message,
+            message: data.result.error.code,
+          });
+        } else {
+          this.localStorageService.setJson('user', data.result.user);
+          this.bartender = data.result.user;
+          this.bartenderProfileForm.patchValue(this.bartender);
+          this.ratings = data.result.ratings;
+        }
+      },
+      (error) => {
+        this.notificationService.displayError(error);
+      },
+    );
+  }
+  onSubmit() {
+    this.restUserService
+    .update(this.bartenderProfileForm.value)
+    .pipe(first())
+    .subscribe(
+      (data: any) => {
+        if (data.result.error) {
+          this.notificationService.displayError({
+            name: data.result.error.message,
+            message: data.result.error.code,
+          });
+        } else {
+          // Success
+          this.notificationService.displaySuccess('User Profile Updated');
+        }
+      },
+      (error) => {
+        this.notificationService.displayError(error);
+      },
+    );  }
 }
--- web-kalimotxo/src/app/pages/bartender/bartender.module.ts
+++ web-kalimotxo/src/app/pages/bartender/bartender.module.ts
@@ -4,8 +4,15 @@ import { ServicesModule } from '@app/services/services.module';
 import { BartenderProfileComponent } from './bartender-profile/bartender-profile.component';
 import { BartenderRoutingModule } from './bartender-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WidgetsModule } from '@app/widgets/widgets.module';
+import { ClarityModule } from '@clr/angular';
+
+import { ApiModule } from '@app/api/api.module';
+
 @NgModule({
-  imports: [ServicesModule, CommonModule, BartenderRoutingModule],
+  imports: [ServicesModule, CommonModule, BartenderRoutingModule, WidgetsModule, ClarityModule,
+    FormsModule, ReactiveFormsModule, ApiModule],
   declarations: [BartenderProfileComponent],
 })
 export class BartenderModule {}
