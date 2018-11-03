08ccfabf2ddcd4375a5612c26d769c51fc327ec1
Bogdan Lupu <lupu60@gmail.com>
2018-11-03 23:17:52 +0200

    fix: bartender profile

--- web-kalimotxo/src/app/pages/bartender/bartender-profile/bartender-profile.component.ts
+++ web-kalimotxo/src/app/pages/bartender/bartender-profile/bartender-profile.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RestUserService } from '@app/api/rest-user.service';
+import { Bartender } from '@app/models/bartender';
+import { Rating } from '@app/models/rating';
 import { BartenderService } from '@app/services/bartender.service';
 import { LocalStorageService } from '@app/services/localStorage.service';
 import { NotificationService } from '@app/services/notification.service';
 import { first } from 'rxjs/operators';
-import { Bartender } from '../../../models/bartender';
-import { Rating } from '../../../models/rating';
-import { Router } from '@angular/router';
-
-import { RestUserService } from '@app/api/rest-user.service';
-
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bartender-profile',
@@ -20,14 +17,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class BartenderProfileComponent implements OnInit {
   bartenderId;
   bartender: Bartender;
+
   ratings: Rating[];
+
   bartenderProfileForm: FormGroup;
-  constructor(private route: ActivatedRoute,
-              private bartenderService: BartenderService,
-              private notificationService: NotificationService,
-              private localStorageService: LocalStorageService,
-              private restUserService: RestUserService,
-              private router: Router) {
+  loading = true;
+  constructor(
+    private route: ActivatedRoute,
+    private bartenderService: BartenderService,
+    private notificationService: NotificationService,
+    private localStorageService: LocalStorageService,
+    private restUserService: RestUserService,
+    private router: Router,
+  ) {
     this.route.params.subscribe((params) => (this.bartenderId = params.bartenderId));
   }
 
@@ -39,48 +41,53 @@ export class BartenderProfileComponent implements OnInit {
       email: new FormControl('', Validators.required),
       phoneNumber: new FormControl('', Validators.required),
       userType: new FormControl('', Validators.required),
-      userId: new FormControl('', Validators.required)
+      userId: new FormControl('', Validators.required),
     });
+
     this.bartenderService
-    .getBartenderById(this.bartenderId)
-    .pipe(first())
-    .subscribe(
-      (data: any) => {
-        if (data.result.error) {
-          this.notificationService.displayError({
-            name: data.result.error.message,
-            message: data.result.error.code,
-          });
-        } else {
-          this.localStorageService.setJson('user', data.result.user);
-          this.bartender = data.result.user;
-          this.bartenderProfileForm.patchValue(this.bartender);
-          this.ratings = data.result.ratings;
-        }
-      },
-      (error) => {
-        this.notificationService.displayError(error);
-      },
-    );
+      .getBartenderById(this.bartenderId)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          if (data.result.error) {
+            this.notificationService.displayError({
+              name: data.result.error.message,
+              message: data.result.error.code,
+            });
+          } else if (data.result.message) {
+            this.notificationService.displayError({
+              message: data.result.message,
+            });
+          } else {
+            this.bartender = data.result.user;
+            this.bartenderProfileForm.patchValue(this.bartender);
+            this.ratings = data.result.ratings;
+            this.loading = false;
+          }
+        },
+        (error) => {
+          this.notificationService.displayError(error);
+        },
+      );
   }
   onSubmit() {
     this.restUserService
-    .update(this.bartenderProfileForm.value)
-    .pipe(first())
-    .subscribe(
-      (data: any) => {
-        if (data.result.error) {
-          this.notificationService.displayError({
-            name: data.result.error.message,
-            message: data.result.error.code,
-          });
-        } else {
-          // Success
-          this.notificationService.displaySuccess('User Profile Updated');
-        }
-      },
-      (error) => {
-        this.notificationService.displayError(error);
-      },
-    );  }
+      .update(this.bartenderProfileForm.value)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          if (data.result.error) {
+            this.notificationService.displayError({
+              name: data.result.error.message,
+              message: data.result.error.code,
+            });
+          } else {
+            this.notificationService.displaySuccess('User Profile Updated');
+          }
+        },
+        (error) => {
+          this.notificationService.displayError(error);
+        },
+      );
+  }
 }
