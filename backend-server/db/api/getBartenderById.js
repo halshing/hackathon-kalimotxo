972bc00a91522f665062ef519e723112c1670c61
Hasan <hasan.alshingieti@gmail.com>
2018-11-03 17:19:57 -0400

    updated bug

--- backend-server/db/api/getBartenderById.js
+++ backend-server/db/api/getBartenderById.js
@@ -3,6 +3,7 @@ const getRatings = require("./getRatingsByBartenderId");
 
 const getBartenderById = async (db, id) => {
   try {
+    console.log(1);
     if (!id)
       return {
         error: "No UserID provided. Please provide a valid UserID.",
@@ -14,19 +15,19 @@ const getBartenderById = async (db, id) => {
       .where(UserType, "==", "bartender")
       .limit(1)
       .get();
-
+    console.log(2);
     if (query.empty)
       return { message: "No bartender was found!", success: false };
-
+    console.log(3);
     // get the ratings for the bartender
     let ratings = await getRatings(db, id);
-    if (ratings.success === false) return { error: { success: false } };
-
+    if (ratings.success === false) ratings = { ratings: [] };
+    console.log(4, ratings);
     // calculate the overall rating for the bartender
     let overallRating = calcAverageRating(ratings);
-
+    console.log(5);
     let user = { ...query.docs[0].data(), overallRating };
-
+    console.log(6);
     return { user, ...ratings };
   } catch (error) {
     return { error };
@@ -34,6 +35,7 @@ const getBartenderById = async (db, id) => {
 };
 
 const calcAverageRating = ({ ratings }) => {
+  if (ratings.length < 1) return null;
   return (
     ratings.reduce((acc, val) => acc + parseInt(val.rating), 0) / ratings.length
   );
