542a4a08920938c362a4953fe1d2ca84d1faec57
Hasan <hasan.alshingieti@gmail.com>
2018-11-03 14:20:42 -0400

    added module to auto generate avatar image

--- backend-server/db/api/createUser.js
+++ backend-server/db/api/createUser.js
@@ -1,30 +1,32 @@
+const avatarUrl = require("../../util/generateAvatar");
 const createUser = async (auth, db, data) => {
-    try {
-        // create Firebase Authentication account
-        let account = await auth
-            .firebase()
-            .createUserWithEmailAndPassword(data.email, data.password);
+  try {
+    // create Firebase Authentication account
+    let account = await auth
+      .firebase()
+      .createUserWithEmailAndPassword(data.email, data.password);
 
-        // we don't want to store passwords - this is handled by Firebase
-        delete data.password;
+    // we don't want to store passwords - this is handled by Firebase
+    delete data.password;
 
-        // add additional properties to our data object
-        data = {
-            ...data,
-            userId: account.user.uid,
-            emailVerified: account.user.emailVerified,
-            creationTime: account.user.metadata.creationTime,
-            lastSignInTime: account.user.metadata.lastSignInTime
-        };
+    // add additional properties to our data object
+    data = {
+      ...data,
+      avatarUrl,
+      userId: account.user.uid,
+      emailVerified: account.user.emailVerified,
+      creationTime: account.user.metadata.creationTime,
+      lastSignInTime: account.user.metadata.lastSignInTime
+    };
 
-        // create the user document
-        let ref = db.doc(`users/${account.user.uid}`);
-        await ref.set(data, { merge: true });
+    // create the user document
+    let ref = db.doc(`users/${account.user.uid}`);
+    await ref.set(data, { merge: true });
 
-        return data;
-    } catch (error) {
-        return { error };
-    }
+    return data;
+  } catch (error) {
+    return { error };
+  }
 };
 
 module.exports = createUser;
