7825eca45ddf3e4df7287a799cb2b26236d7776d
Hasan <hasan.alshingieti@gmail.com>
2018-11-02 22:06:34 -0400

    added user profile in response

--- backend-server/db/api/signIn.js
+++ backend-server/db/api/signIn.js
@@ -1,7 +1,15 @@
-const signIn = async (auth, data) => {
-  return await auth
-    .firebase()
-    .signInWithEmailAndPassword(data.email, data.password);
+const signIn = async (auth, db, data) => {
+  try {
+    let result = await auth
+      .firebase()
+      .signInWithEmailAndPassword(data.email, data.password);
+    let ref = db.doc(`users/${result.user.uid}`);
+    let user = await ref.get();
+    if (!user.exists) return { message: "User does not exist!" };
+    return { user: user.data() };
+  } catch (error) {
+    return { error };
+  }
 };
 
 module.exports = signIn;
