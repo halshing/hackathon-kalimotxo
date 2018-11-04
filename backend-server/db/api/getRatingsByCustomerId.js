8b86164c5fc0482b5e994a7b0f65fcb86344e3cc
Hasan <hasan.alshingieti@gmail.com>
2018-11-04 08:37:28 -0500

    fixed message text

--- backend-server/db/api/getRatingsByCustomerId.js
+++ backend-server/db/api/getRatingsByCustomerId.js
@@ -7,7 +7,7 @@ const getRatingsByCustomerId = async (db, id) => {
             .get();
 
         if (query.empty)
-            return { message: "No ratings for that bartender found", success: false };
+            return { message: "No ratings for that customer found", success: false };
 
         let ratings = query.docs.map(doc => doc.data());
 
