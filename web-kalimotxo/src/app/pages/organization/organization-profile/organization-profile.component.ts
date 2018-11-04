c13093aac4f09c62ff4bf072d7749d8dbfc86716
Bogdan Lupu <lupu60@gmail.com>
2018-11-04 18:11:30 +0200

    add bussines profile

--- web-kalimotxo/src/app/pages/organization/organization-profile/organization-profile.component.ts
+++ web-kalimotxo/src/app/pages/organization/organization-profile/organization-profile.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { RestCustomerService } from '@app/api/rest-customer.service';
+import { RestUserService } from '@app/api/rest-user.service';
+import { User } from '@app/models/users';
+import { NotificationService } from '@app/services/notification.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organization-profile',
@@ -7,10 +13,73 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./organization-profile.component.less'],
 })
 export class OrganizationProfileComponent implements OnInit {
-  orgId;
-
-  constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe((params) => (this.orgId = params.orgId));
+  customerId;
+  customer: User;
+  customerProfileForm: FormGroup;
+  loading = true;
+  constructor(
+    private route: ActivatedRoute,
+    private notificationService: NotificationService,
+    private restCustomerService: RestCustomerService,
+    private restUserService: RestUserService,
+  ) {
+    this.route.params.subscribe((params) => (this.customerId = params.customerId));
   }
-  ngOnInit() {}
+
+  ngOnInit() {
+    this.customerProfileForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', Validators.required),
+      userType: new FormControl('', Validators.required),
+      userId: new FormControl('', Validators.required),
+    });
+    this.restCustomerService
+      .getCustomerById(this.customerId)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          if (data.result.error) {
+            this.notificationService.displayError({
+              name: data.result.error.message,
+              message: data.result.error.code,
+            });
+          } else if (data.result.message) {
+            this.notificationService.displayError({
+              name: data.result.error.message,
+              message: data.result.message,
+            });
+          } else {
+            this.customer = data.result.user;
+            this.customerProfileForm.patchValue(this.customer);
+            this.loading = false;
+          }
+        },
+        (error) => {
+          this.notificationService.displayError(error);
+        },
+      );
+  }
+  onSubmit() {
+    this.restUserService
+      .update(this.customerProfileForm.value)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          if (data.result.error) {
+            this.notificationService.displayError({
+              name: data.result.error.message,
+              message: data.result.error.code,
+            });
+          } else {
+            this.notificationService.displaySuccess('Customer Profile Updated');
+          }
+        },
+        (error) => {
+          this.notificationService.displayError(error);
+        },
+      );
+  }
 }
